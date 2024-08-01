3133537acd43bca84b620f38e54fadb20a76da70
Minionguyjpro <minionguyjpro@gmail.com>
2024-08-01 14:25:19 +0200

    Add device.mk
    Signed-off-by: Minionguyjpro <minionguyjpro@gmail.com>

---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/gtowifi
+
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
